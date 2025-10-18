d1c2caae078a1abd426bcb04e595ee7d5c2a7065
joeychuavertex <97017755+joeychuavertex@users.noreply.github.com>
2025-10-18 19:29:17 +0800

    use type for auth

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -2,7 +2,7 @@
 
 import React, { createContext, useContext, ReactNode } from 'react'
 import { useAuth } from '@/hooks/useAuth'
-import { AuthUser, AuthState } from '../lib/types'
+import { AuthUser, AuthState } from '@/types/auth'
 
 interface AuthContextType extends AuthState {
   user: AuthUser | null
