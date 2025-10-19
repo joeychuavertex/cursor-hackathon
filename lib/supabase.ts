4211a31883707d74da39503767153749a22bcd8d
joeychuavertex <97017755+joeychuavertex@users.noreply.github.com>
2025-10-19 11:33:50 +0800

    catch error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/database'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
+}
+
+if (!supabaseKey) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
+}
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: {
